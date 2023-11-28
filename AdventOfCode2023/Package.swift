10d9a47834d4c0248419f5c31a4b631c3db650d8
Andre Stenvall <andre.stenvall@wearelevels.com>
2023-11-28 17:04:25 +0100

    Prepare for Advent of Code 2023

---
+++ AdventOfCode2023/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AdventOfCode2023",
+    platforms: [
+        .macOS(.v14)
+    ],
+    products: [
+        // .executable(
+        //     name: "Day1",
+        //     targets: ["Day1"]
+        // )
+    ],
+    dependencies: [],
+    targets: [
+        // .executableTarget(
+        //     name: "Day1",
+        //     dependencies: [],
+        //     resources: [
+        //         .copy("input.txt")
+        //     ]
+        // )
+    ]
+)
---
+++ AdventOfCode2023/README.md
@@ -0,0 +1,6 @@
+# Advent of code 2023
+
+```
+> swift run <Day1|Day2|...> 
+```
+
